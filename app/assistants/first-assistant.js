db73cd7a543cd0d052124d610512c7e5eff2c60e
Darrin Holst <darrinholst@gmail.com>
2011-01-19 15:22:06 -0600

    update

--- app/assistants/first-assistant.js
+++ app/assistants/first-assistant.js
@@ -1,4 +1,8 @@
 var FirstAssistant = Class.create({
+  initialize: function() {
+    this.results = {}
+  },
+
   setup: function() {
     this.controller.listen("start", Mojo.Event.tap, this.start = this.start.bind(this))
     this.controller.listen("stop", Mojo.Event.tap, this.stop = this.stop.bind(this))
@@ -11,8 +15,21 @@ var FirstAssistant = Class.create({
 
   calculateSha1: function(iteration) {
     if(this.stillGoing) {
-      console.log(core_sha1([1,2,3,4,5], 160))
-      this.controller.get("results").update(iteration)
+      var sha1 = core_sha1([1,2,3,4,5], 160)
+      var result = "" + sha1[0]
+      var count = this.results[result] || 0
+      this.results[result] = (count + 1)
+      var div = this.controller.get("results")
+      div.update("<table>")
+
+      for(var r in this.results) {
+        if(this.results.hasOwnProperty(r)) {
+          div.insert('<tr><td class="count">' + this.results[r] + '</td><td>' + r + '</td></tr>')
+        }
+      }
+
+      div.insert("</table>")
+
       setTimeout(this.calculateSha1.bind(this, iteration + 1), 100)
     }
   },
